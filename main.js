62c500ce9b3637daa7a2c3915e7adeded03f4fb9
ewahjelm <ewahjelm84@gmail.com>
2024-12-09 22:16:11 +0100

    påbörjat createAnswerButtons

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
-/* Vad ska köras onload?:
-welcome()
-startbutton
+/* Vad ska köras hur?:
+onload
+welcome() display text & startbutton
+  startbutton, onclick:
+  remove welcome-block
+  create & append questionblock
 
 */
-var spaDiv = document.getElementById("root")
+const spaDiv = document.getElementById("root")
 const quizBankEndpoint = "ordQuiz.json"
 const elementBox = document.createElement("section");
 elementBox.id = "question-block";
+const quizData = [];
+
+//hämtar frågebank i bakgrunden från JSON-filen
+const getQuizBank = async () => {
+    const quizBank = await fetch(quizBankEndpoint);
+    if (quizBank.status !== 200) {
+        throw new Error("Kan inte hitta datan. Kolla att du har rätt endpoint i anropet.")
+    }
+    quizData = await quizBank.json();
+    return quizData;
+};
+/* .then(response => response.json())
+
+.then(quizData => runQuiz(quizData)); */
+
+
 
 function welcome(spaDiv) {
     spaDiv.append(elementBox);
@@ -28,35 +47,35 @@ function welcome(spaDiv) {
         e.preventDefault();
         console.log("du har klickat")
         clearDiv();
-        showQuestionBlock();
+        showQuestionBlock(quizData); //funkar inte än
+        return quizData;
     })
 }
 
 
-//hämtar frågebank från JSON-filen
-const getQuizBank = async () => {
-    const quizBank = await fetch(quizBankEndpoint);
-    if (quizBank.status !== 200) {
-        throw new Error("Kan inte hitta datan. Kolla att du har rätt endpoint i anropet.")
+
+function createAnswerButtons() {
+    const answerButtons = [];
+    for (let i = 1; i < 5; i++) {
+        const button = document.createElement("button");
+        button.className = "button"
+        button.id = `answer${i}`;
+        answerButtons.push(button);
     }
-    const quizData = await quizBank.json();
-    return quizData;
-};
-/* .then(response => response.json())
 
-.then(quizData => runQuiz(quizData)); */
+}
 
-
-function showQuestionBlock() {
+function showQuestionBlock(quizData) {
     spaDiv.append(elementBox);
 
-    /*  for (i = 1; i < 5; i++) {
-         const answerButton = document.createElement("button");
-         answerButton.className = "button"
-         answerButton.id = `answer${i}`;
-     
-     } */
+    const questionText = "";
+    console.log(quizData);
+
+    // questionText = runQuiz(quizData);
+
+    elementBox.append();
 }
+
 function clearDiv() {
     spaDiv.remove(elementBox);
 }
@@ -68,6 +87,8 @@ function runQuiz(q) {
     console.log(q[index].question)
 
 
+
 };
 
 welcome(spaDiv);
+createAnswerButtons();
